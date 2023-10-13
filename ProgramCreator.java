1618ade9b65c5b802537e29168158a1fa6850946
Egor <145351359+LeopoldJerry@users.noreply.github.com>
2023-10-13 15:12:25 +0600

    Add files via upload

---
+++ ProgramCreator.java
@@ -0,0 +1,13 @@
+
+public class ProgramCreator {
+    public static void main(String[] args) {
+        Program [] programs = {
+            new BankSystem(new CppDeveloper()),
+                new StockExchange(new JavaDeveloper())
+        };
+
+        for (Program program: programs){
+            program.developProgram();
+        }
+    }
+}
